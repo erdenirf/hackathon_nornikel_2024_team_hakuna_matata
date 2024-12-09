7bc73bfad845c3cb0e920191d9fa647f7f0db02e
Erdeni Tsyrendashiev <erdenirf@gmail.com>
2024-12-10 02:20:25 +0300

    from PIL import Image #2

--- src/pdf2image_loader.py
+++ src/pdf2image_loader.py
@@ -14,7 +14,7 @@ except ImportError:
     )
 
 try:
-    from PIL.Image import Image
+    from PIL import Image
 except ImportError:
     raise ImportError(
         "PIL package not found, please install it with `pip install Pillow`"
@@ -26,7 +26,7 @@ from pathlib import Path
 class Pdf2ImageLoader(BaseLoader):
     file_path: str
     metadata: dict
-    images: list[Image]
+    images: list[Image.Image]
 
     def __init__(self, file_path: str):
 
@@ -39,7 +39,7 @@ class Pdf2ImageLoader(BaseLoader):
         self.images = convert_from_path(self.file_path, fmt='jpeg', dpi=100, thread_count=4)
         return self.convert_Images_to_Documents(self.images, self.metadata)
     
-    def convert_Image_to_base64(self, image: Image) -> str:
+    def convert_Image_to_base64(self, image: Image.Image) -> str:
         import base64
         import io
         # Save image to bytes buffer
@@ -51,7 +51,7 @@ class Pdf2ImageLoader(BaseLoader):
         base64_str = f"data:image/jpeg;base64,{base64_data}"
         return base64_str
 
-    def convert_Images_to_Documents(self, images: list[Image], metadata: dict) -> list[Document]:
+    def convert_Images_to_Documents(self, images: list[Image.Image], metadata: dict) -> list[Document]:
         ret_docs: list[Document] = []
         for index, image in enumerate(images):
             page_content = self.convert_Image_to_base64(image.convert('RGB'))
